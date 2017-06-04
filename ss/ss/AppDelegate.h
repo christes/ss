5b8252aa28293d2939efa7babdb28a41542f2b51
christes <christe930112@gmail.com>
2017-06-04 22:11:49 +0800

    新pod

---
+++ ss/ss/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ss
+//
+//  Created by boundlessocean on 2017/6/4.
+//  Copyright © 2017年 Lemon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
